365d3d0d1fd68a84750db68f62e7b0e6b3cda18e
mostafizurrahman <mostafizur.cse@gmail.com>
2022-11-04 03:33:29 +0600

    Created project for object detection

---
+++ object_detection/object_detection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  object_detection
+//
+//  Created by mostafizur_admin on 4/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
